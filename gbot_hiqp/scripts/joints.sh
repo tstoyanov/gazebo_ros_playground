65095c7aa3d450fcfb41c90a563c679483d3a55a
Todor Stoyanov <todor.stoyanov@oru.se>
2020-07-14 08:19:13 +0200

    tried adding one more preload task

--- gbot_hiqp/scripts/joints.sh
+++ gbot_hiqp/scripts/joints.sh
@@ -5,5 +5,5 @@ rosservice call /hiqp_joint_effort_controller/set_tasks \
   visible: 1
   active: 1
   monitored: 1
-  def_params: ['TDefFullPose', '0.0', '-1.17', '0.003']  
+  def_params: ['TDefFullPose', '0.5', '-1.97', '-1.883']  
   dyn_params: ['TDynPD', '1.0', '2.0'] "
